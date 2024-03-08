af3332beb5ea14d3d4373566fc0d8d82ae54efcb
hankpillow <igor.p.almeida@gmail.com>
2024-03-08 09:05:08 -0300

    coverage setup

--- vitest.config.js
+++ vitest.config.js
@@ -2,8 +2,11 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  test: {
-    globals: true,
-    environment:"happy-dom"
-  },
+    test: {
+        globals: true,
+        environment: 'happy-dom',
+        coverage: {
+            exclude: ['site', '*.d.ts'],
+        },
+    },
 })
